d373bcd6da297658935bc6c449798eb9564f8358
PC-DOS <suicunexp@outlook.com>
2023-04-04 17:42:53 +0800

    Code formatting

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,42 +3,39 @@
 #include <QString>
 #include <QTimer>
 
-int iTotal=0;
-int iCurrent=0;
-int iFps=1;
+int iTotal = 0;
+int iCurrent = 0;
+int iFps = 1;
 QTimer * tmrFpsCounter;
 QTimer * tmrGenerator;
 
-void MainWindow::CountTotal(){
+void MainWindow::CountTotal() {
     ++iTotal;
     ++iCurrent;
-    ui->lblTotal->setText(QString::number(iTotal)+QString(" Counts"));
-    ui->lblFps->setText(QString::number(iFps)+QString(" fps"));
+    ui->lblTotal->setText(QString::number(iTotal) + QString(" Counts"));
+    ui->lblFps->setText(QString::number(iFps) + QString(" fps"));
 }
 
-void MainWindow::CalcFps(){
-    iFps=iCurrent;
-    iCurrent=0;
-    ui->lblTotal->setText(QString::number(iTotal)+QString(" Counts"));
-    ui->lblFps->setText(QString::number(iFps)+QString(" fps"));
+void MainWindow::CalcFps() {
+    iFps = iCurrent;
+    iCurrent = 0;
+    ui->lblTotal->setText(QString::number(iTotal) + QString(" Counts"));
+    ui->lblFps->setText(QString::number(iFps) + QString(" fps"));
 }
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent),
+                                           ui(new Ui::MainWindow) {
     ui->setupUi(this);
-    ui->lblTotal->setText(QString::number(iTotal)+QString(" Counts"));
-    ui->lblFps->setText(QString::number(iFps)+QString(" fps"));
-    tmrFpsCounter=new QTimer(this);
+    ui->lblTotal->setText(QString::number(iTotal) + QString(" Counts"));
+    ui->lblFps->setText(QString::number(iFps) + QString(" fps"));
+    tmrFpsCounter = new QTimer(this);
     connect(tmrFpsCounter, SIGNAL(timeout()), this, SLOT(CalcFps()));
     tmrFpsCounter->start(1000);
-    tmrGenerator=new QTimer(this);
+    tmrGenerator = new QTimer(this);
     connect(tmrGenerator, SIGNAL(timeout()), this, SLOT(CountTotal()));
     tmrGenerator->start(0);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,15 +4,14 @@
 #include <QMainWindow>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget * parent = 0);
     ~MainWindow();
 
 private slots:
@@ -20,7 +19,7 @@ private slots:
     void CalcFps();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow * ui;
 };
 
 #endif // MAINWINDOW_H
